ae766da6d7d3af9bdf2f1f9e34e6705728327602
Sandeepdevops22 <sandeepmgvr@gmail.com>
2026-02-01 15:49:00 +0530

    Delete s3/backend.tf

--- s3/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "sandeepmgvr1234-terraform-state"
-    key            = "sandeep_team/dev/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
